a654ff9ca47410efc33097b04266ff25a481568c
MartinVelez2002 <96700377+MartinVelez2002@users.noreply.github.com>
2024-10-27 10:28:59 -0500

    Se corrigió error de Prioridad eliminada

--- Autenticacion/Auth/urls.py
+++ Autenticacion/Auth/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the-include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from Auth import settings
@@ -21,7 +20,6 @@ from Auth import settings
 urlpatterns = [
 
     path('', include('Modulos.Login.urls')),
-    path('prioridad/', include('Modulos.Prioridad.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Autenticacion/Modulos/Incidente/models.py
+++ Autenticacion/Modulos/Incidente/models.py
@@ -2,14 +2,13 @@ from django.db import models
 from Modulos.Ubicacion.models import Ubicacion_Model
 from Modulos.Login.models import Usuario
 from Modulos.Novedad.models import Novedad_Model
-from Modulos.Prioridad.models import Prioridad_Model
+
 # Create your models here.
 class CabIncidente_Model(models.Model):
     Lugar = models.ForeignKey(Ubicacion_Model, on_delete = models.PROTECT)
     Agente = models.ForeignKey(Usuario, on_delete = models.PROTECT, name = 'Agente', related_name = 'agente')
     Supervisor = models.ForeignKey(Usuario, on_delete = models.PROTECT, name = 'Supervisor', related_name = 'supervisor')
     Novedad = models.ForeignKey(Novedad_Model, on_delete = models.PROTECT)
-    Prioridad = models.ForeignKey(Prioridad_Model, on_delete = models.PROTECT)
     Fecha = models.DateField(auto_now = False)
     Estado = models.BooleanField(default = True)
     
