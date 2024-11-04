fded602b51fafdfe8f52d3fdfbad2b863e1d3cbb
AndresM023 <andresmolina032@outlook.es>
2024-11-04 08:45:42 -0500

    .

--- Autenticacion/Modulos/Login/urls.py
+++ Autenticacion/Modulos/Login/urls.py
@@ -7,12 +7,19 @@ from django.urls import path
 app_name = 'login'
 urlpatterns = [
     path('ajustes/', login_required(admin.site.urls)),
+    
     path('',MainView.as_view(),name = 'index'),
+    
     path('accounts/login/',Login.as_view(), name='login'),
-    path('logout',login_required(LogoutUsuario),name = 'logout'),
-    path('registro',RegistroView.as_view(),name = 'registro'),
+    
+    path('logout/',login_required(LogoutUsuario),name = 'logout'),
+    
+    path('registro/',RegistroView.as_view(),name = 'registro'),
+    
     path('clave_olvidar/',ForgetPassword.as_view(),name='olvidar_clave'),
+    
     path('restablecer_clave/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='restablecer_clave'),
+    
     path('cambiar_clave/',ChangePasswordFirstSession.as_view(), name = 'cambiar_clave')
 ]
 
