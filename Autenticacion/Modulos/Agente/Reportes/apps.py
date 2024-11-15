b5d6ec96b3bbca78cc97f0a6ab3a9546bc39f367
MartinVelez2002 <96700377+MartinVelez2002@users.noreply.github.com>
2024-11-15 15:05:03 -0500

    .

--- Autenticacion/Modulos/Agente/Reportes/apps.py
+++ Autenticacion/Modulos/Agente/Reportes/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 class ReportesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Modulos.Agente.Reportes'
-    
+
     def ready(self):
-        import Reportes.signals  # Importa las señales al iniciar la aplicación
+        import Modulos.Agente.Reportes.signals  # Importa las señales al iniciar la aplicación
