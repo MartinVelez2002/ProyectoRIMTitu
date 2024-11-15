f1a2cfb87422a5adb8e58409dab963033a35933a
MartinVelez2002 <martinvelezjr@hotmail.com>
2024-11-14 23:53:22 -0500

    Plantilla de reportes

--- Autenticacion/Modulos/Agente/Reportes/forms.py
+++ Autenticacion/Modulos/Agente/Reportes/forms.py
@@ -5,17 +5,16 @@ class Reportes_Form_C(forms.ModelForm):
     class Meta:
         model = Reportes_Model
         fields = ['novedad', 'prioridad', 'evidencia', 'descripcion']
-        # widgets = {
-        #     'descripcion': forms.Textarea(),
-        # }
+        widgets = {
+            'descripcion': forms.TextInput(),
+        }
     def __init__(self, *args, **kwargs):
         super(Reportes_Form_C, self).__init__(*args, **kwargs)
         for field in self.fields.values():
-            if field.name != 'descripcion':
-                field.widget.attrs.update({
-                    'class': 'input',
-                    'required': True
-                })
+            field.widget.attrs.update({
+                'class': 'input',
+                'required': True
+            })
 
 
     def clean_evidencia(self):
@@ -24,6 +23,6 @@ class Reportes_Form_C(forms.ModelForm):
         # Verifica el tipo MIME (opcional)
         valid_mimetypes = ['image/jpeg', 'image/png', 'image/gif', 'video/mp4', 'video/quicktime', 'video/x-msvideo']
         if evidencia and evidencia.content_type not in valid_mimetypes:
-            self.add_error("Tipo de archivo no permitido.")
+            self.add_error('evidencia', "Tipo de archivo no permitido.")
         
         return evidencia
