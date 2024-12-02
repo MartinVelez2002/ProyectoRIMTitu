ae1785f73a1aeeb481ee2cd1089958979c2ce0a5
MartinVelez2002 <96700377+MartinVelez2002@users.noreply.github.com>
2024-12-02 17:41:15 -0500

    Modificaciones para el deploy

--- Autenticacion/build_files.sh
+++ Autenticacion/build_files.sh
@@ -1,3 +1,13 @@
-# build_files.sh
+#!/bin/bash
+
+# Instalar dependencias
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+# Recolectar archivos estáticos
+python3.9 manage.py collectstatic --noinput
+
+# Crear el directorio de salida para los archivos estáticos
+mkdir -p staticfiles_build
+
+# Mover los archivos recolectados al directorio de salida
+cp -r staticfiles/* staticfiles_build/
